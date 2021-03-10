2aab7c70e81f8340d10360117626eb36baa9ccff
vicdata <vicdata@outlook.com>
2021-03-10 23:45:26 +0100

    removed docs build

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,11 +39,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend(config, { isClient }) {
-      // Extend only webpack config for client-bundle
-      if (isClient) {
-        config.devtool = 'source-map'
-      }
-    }
   }
 }
